728bd056e423833ea1120e44aba60519d929ea6e
jgsqware <garciagonzalez.julien@gmail.com>
2016-08-18 16:09:36 +0200

    cleaning

--- emacs/.emacs.d/config/ruby.el
+++ emacs/.emacs.d/config/ruby.el
@@ -16,10 +16,6 @@
 (package-install 'robe)
 (add-hook 'ruby-mode-hook 'robe-mode)
 
-;;pry section
-(add-to-list 'load-path "~/.emacs.d/vendor/emacs-pry")
-(require 'pry)
-
 ;; ruby-mode section
 (add-to-list 'auto-mode-alist
 	     '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . ruby-mode))
