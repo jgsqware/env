dfb6165099d420e283859a79287f43fb9ebfb5fd
jgsqware <garciagonzalez.julien@gmail.com>
2016-08-18 16:07:48 +0200

    update rubocop

--- emacs/.emacs.d/config/ruby.el
+++ emacs/.emacs.d/config/ruby.el
@@ -3,9 +3,14 @@
 (require 'package)
 
 ;;; Code:
-(package-install 'flycheck)
 (package-install 'inf-ruby)
+
+;;flycheck section
+(package-install 'flycheck)
 (global-flycheck-mode)
+
+(with-eval-after-load 'flycheck
+  (flycheck-add-mode 'ruby-rubocop 'ruby-mode))
 
 ;;robe section
 (package-install 'robe)
@@ -18,6 +23,7 @@
 ;; ruby-mode section
 (add-to-list 'auto-mode-alist
 	     '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . ruby-mode))
+
 
 (provide 'ruby)
 ;;; ruby.el ends here
