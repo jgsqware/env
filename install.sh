987e8f156f17cd406f44f0567ead2d7c8b851a4d
Julien Garcia Gonzalez <julien.garciagonzalez@lampiris.be>
2017-08-17 16:44:33 +0200

    update for arch

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+setupEnv() {
+  echo 'Copy SSH key from Enpass BEFORE CONTINUING'
+  echo 'Press Enter to continue'
+  read secondyn </dev/tty
+  
+  chmod 400 ~/.ssh/id_rsa
+  
+  echo 'Clone jgsqware/env'
+  git clone git@github.com:jgsqware/env.git ~/workspace/github/jgsqware/env
+  
+  echo 'Go to ansible folder and run ansible: '
+  echo '$ cd $WORKSPACE/github/jgsqware/env/ansible'
+  echo '$ ansible-playbook -K -i hosts playbook.yml'
+}
 
 ubuntuInstall() {
   echo "Ubuntu flavor installation"
@@ -17,22 +31,19 @@ ubuntuInstall() {
     enpass \
     ansible 
   
-  echo 'Copy SSH key from Enpass BEFORE CONTINUING'
-  echo 'Press Enter to continue'
-  read secondyn </dev/tty
-  
-  chmod 400 ~/.ssh/id_rsa
-  
-  echo 'Clone jgsqware/env'
-  git clone git@github.com:jgsqware/env.git ~/workspace/github/jgsqware/env
-  
-  echo 'Go to ansible folder and run ansible: '
-  echo '$ cd $WORKSPACE/github/jgsqware/env/ansible'
-  echo '$ ansible-playbook -K -i hosts playbook.yml'
+  setupEnv
 }
 
 archInstall() {
   echo "Arch flavor installation"
+
+  echo 'Install Enpass'
+  yaourt -S --noconfirm aur/enpass-bin
+
+  echo 'Install ansible'
+  pacman -S --noconfirm enpass
+
+  setupEnv
 }
 
 DISTRIB_ID=$(cat /etc/*-release | grep -w ID | awk -F "=" '{print $2}' | sed 's/"//g') 
